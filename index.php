b4439ebdb2c87d8b2f25c58e8cfad810b5dc5aca
Frank Kalmbach <frank.kalmbach@hotmail.com>
2015-06-30 23:20:46 -0400

    Added autoload, composer, monolog and slim to project

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+require 'vendor/autoload.php';
 
-echo 'Hello World!';
+//setting local timezone for log
+date_default_timezone_set('America/Kentucky/Louisville');
+
+//creating an 'alias' for Logger so can reduce repeat use
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+//creating a new object variable for logging with following methods:
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+
+$log->addWarning('Foo');
+$log->addError('Bar');
+
+echo 'Hello, World!';
 
 
 
